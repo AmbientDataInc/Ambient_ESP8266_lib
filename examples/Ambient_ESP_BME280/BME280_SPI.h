748370e9330735d385e1b617a623b23ee5be087e
Takehiko Shimojima <shimojima@gmail.com>
2018-01-11 09:59:46 +0900

    ESPr DeveloperとBME280で温度、湿度、気圧を測定し、Ambientに送信する

---
+++ examples/Ambient_ESP_BME280/BME280_SPI.h
@@ -0,0 +1,51 @@
+/*
+ * Modified from Switchscience BME280 sample code.
+ *  * http://trac.switch-science.com/wiki/BME280
+ */
+#ifndef MBED_BME280_H
+#define MBED_BME280_H
+
+class BME280
+{
+public:
+    BME280(void);
+
+    virtual ~BME280();
+
+    void begin(int cs);
+
+    double readTemperature(void);
+
+    double readHumidity(void);
+
+    double readPressure(void);
+
+private:
+    signed long int t_fine;
+
+    uint16_t dig_T1;
+     int16_t dig_T2;
+     int16_t dig_T3;
+    uint16_t dig_P1;
+     int16_t dig_P2;
+     int16_t dig_P3;
+     int16_t dig_P4;
+     int16_t dig_P5;
+     int16_t dig_P6;
+     int16_t dig_P7;
+     int16_t dig_P8;
+     int16_t dig_P9;
+     int8_t  dig_H1;
+     int16_t dig_H2;
+     int8_t  dig_H3;
+     int16_t dig_H4;
+     int16_t dig_H5;
+     int8_t  dig_H6;
+
+    void writeReg(uint8_t reg_address, uint8_t data);
+    void readReg(uint8_t reg_address, int numBytes,  uint8_t * data);
+    void readTrim(void);
+    int bme280_cs;
+};
+
+#endif // MBED_BME280_H
